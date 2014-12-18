6c138dee0e8d0b3b6ca776c2cae893debf66ed2b
xxiao <843814555@qq.com>
2014-12-18 13:23:47 -0500

    delet tllofunction

--- getScores.js
+++ getScores.js
@@ -1,11 +1,12 @@
 function gapFilling() {
   var gapFillingscore = 0;
-  gapFillingscore += contrastAnswer(document.getElementById('1_1').value,'统一建模语言',5);
-  gapFillingscore += contrastAnswer(getElementById('1_2_1').value,'封装性',5);
-  gapFillingscore += contrastAnswer(getElementById('1_2_2').value,'继承性',5);
-  gapFillingscore += contrastAnswer(getElementById('1_2_3').value,'多态性',5);
+  gapFillingscore += contrastAnswer(document.getElementById('1_1').value,'统一建模语言',1);
+  gapFillingscore += contrastAnswer(document.getElementById('1_2_1').value,'封装性',1);
+  gapFillingscore += contrastAnswer(document.getElementById('1_2_2').value,'继承性',1);
+  gapFillingscore += contrastAnswer(document.getElementById('1_2_3').value,'多态性',1);
   return gapFillingscore;
 }
+
 
 function singleChoice() {
   var singleChoicescore = 0;
@@ -13,14 +14,15 @@ function singleChoice() {
   var answer_2_2 = document.getElementsByName('2_2');
   for(var i = 0; i < answer_2_1.length; i++) {
     if (answer_2_1[i].checked && answer_2_1[i].value === 'B' ) {
-      singleChoicescore += 15;
+      singleChoicescore += 10;
     }
     if (answer_2_2[i].checked && answer_2_2[i].value === 'A' ) {
-      singleChoicescore += 15;
+      singleChoicescore += 10;
     }
   }
   return singleChoicescore;
 }
+
 
 function multipleChoice() {
   var multipleChoicescore = 0;
@@ -42,7 +44,7 @@ function multipleChoice() {
   }
 
   if (answer_3_1_A + answer_3_1_B + answer_3_1_C + answer_3_1_D === 'ABD') {
-    multipleChoicescore += 15;
+    multipleChoicescore += 100;
   }
 
   var answer_3_2_A = '';
@@ -63,35 +65,31 @@ function multipleChoice() {
   }
 
   if (answer_3_2_A + answer_3_2_B + answer_3_2_C + answer_3_2_D === 'ABC') {
-    multipleChoicescore += 15;
+    multipleChoicescore += 100;
   }
   return multipleChoicescore;
 
 }
+
 
 function trueAndFalse() {
   var trueAndFalsescore = 0;
   var answer_4_1 = document.getElementById('4_1');
   var answer_4_2 = document.getElementById('4_2');
   if (answer_4_1.value === '错' ) {
-    trueAndFalsescore += 5;
+    trueAndFalsescore += 1000;
   }
   if (answer_4_2.value === '对' ) {
-    trueAndFalsescore += 5;
+    trueAndFalsescore += 1000;
   }
   return trueAndFalsescore;
 }
 
+
 function question() {
   var questionscore = 0;
-  questionscore += contrastAnswer(document.getElementById('5').value,'模型是对现实世界的简化和抽象，模型是对所研究的系统、过程、事物或概念的一种表达形式；可以是某种图形；或者是一种数学表达式。',35);
+  questionscore += contrastAnswer(document.getElementById('5').value,'模型是对现实世界的简化和抽象，模型是对所研究的系统、过程、事物或概念的一种表达形式；可以是某种图形；或者是一种数学表达式。',10000);
   return questionscore;
-  //   var score = 0;
-  //   var answer_5 = document.getElementById('5');
-  //   if (answer_5.value === '模型是对现实世界的简化和抽象，模型是对所研究的系统、过程、事物或概念的一种表达形式；可以是某种图形；或者是一种数学表达式。' ) {
-  //     score += 35;
-  //   }
-  //   return score;
 }
 
 
@@ -99,123 +97,7 @@ function contrastAnswer(userAnswer,answer,score) {
   if (userAnswer === answer) {
     return score;
   }
-  return 0;
+  else {
+    return 0;
+  }
 }
-
-//ContrastAnswer.gapFilling = function() {
-  //var gapFillingScore = 0;
-  // for (var i = 0; i < this.answer.length; i++) {
-  //   if (this.userAnswer[i] === this.answer[i]) {
-  //     gapFillingScore += this.score[i];
-  //   }
-//   //}
-//   var score = 0;
-//   var answer_1_1 = document.getElementById('1_1');
-//   var answer_1_2_1 = document.getElementById('1_2_1');
-//   var answer_1_2_2 = document.getElementById('1_2_2');
-//   var answer_1_2_3 = document.getElementById('1_2_3');
-//   if (answer_1_1.value === '统一建模语言') {
-//     score += 10;
-//   }
-//   if (answer_1_2_1.value === '封装性') {
-//     score += 10;
-//   }
-//   if (answer_1_2_2.value === '继承性') {
-//     score += 10;
-//   }
-//   if (answer_1_2_3.value === '多态性') {
-//     score += 10;
-//   }
-//   return score;
-// };
-//
-//
-// ContrastAnswer.singleChoice = function() {
-//   var score = 0;
-//   var answer_2_1 = document.getElementsByName('2_1');
-//   var answer_2_2 = document.getElementsByName('2_2');
-//   for(var i = 0; i < answer_2_1.length; i++) {
-//     if (answer_2_1[i].checked && answer_2_1[i].value === 'B' ) {
-//       score += 15;
-//     }
-//     if (answer_2_2[i].checked && answer_2_2[i].value === 'A' ) {
-//       score += 15;
-//     }
-//   }
-//   return score;
-// };
-//
-//
-//
-//
-// ContrastAnswer.multipleChoice = function() {
-//   var score = 0;
-//
-//   var answer_3_1_A = '' ;
-//   var answer_3_1_B = '';
-//   var answer_3_1_C = '';
-//   var answer_3_1_D = '';
-//   if (document.getElementById('3_1_A').checked) {
-//     answer_3_1_A = 'A';
-//   }
-//   if (document.getElementById('3_1_B').checked) {
-//     answer_3_1_B = 'B';
-//   }
-//   if (document.getElementById('3_1_C').checked) {
-//     answer_3_1_C = 'C';
-//   }
-//   if (document.getElementById('3_1_D').checked) {
-//     answer_3_1_D = 'D';
-//   }
-//
-//   if (answer_3_1_A + answer_3_1_B + answer_3_1_C + answer_3_1_D === 'ABD') {
-//     score += 15;
-//   }
-//
-//
-//   var answer_3_2_A = '';
-//   var answer_3_2_B = '';
-//   var answer_3_2_C = '';
-//   var answer_3_2_D = '';
-//   if (document.getElementById('3_2_A').checked) {
-//     answer_3_2_A = 'A';
-//   }
-//   if (document.getElementById('3_2_B').checked) {
-//     answer_3_2_B = 'B';
-//   }
-//   if (document.getElementById('3_2_C').checked) {
-//     answer_3_2_C = 'C';
-//   }
-//   if (document.getElementById('3_2_D').checked) {
-//     answer_3_2_D = 'D';
-//   }
-//
-//   if (answer_3_2_A + answer_3_2_B + answer_3_2_C + answer_3_2_D === 'ABC') {
-//     score += 15;
-//   }
-//   return score;
-// };
-//
-//
-// ContrastAnswer.trueAndFalse = function() {
-//   var score = 0;
-//   var answer_4_1 = document.getElementById('4_1');
-//   var answer_4_2 = document.getElementById('4_2');
-//   if (answer_4_1.value === '错' ) {
-//     score += 5;
-//   }
-//   if (answer_4_2.value === '对' ) {
-//     score += 5;
-//   }
-//   return score;
-// };
-//
-//
-// ContrastAnswer.question = function() {
-//   var score = 0;
-//   var answer_5 = document.getElementById('5');
-//   if (answer_5.value === '模型是对现实世界的简化和抽象，模型是对所研究的系统、过程、事物或概念的一种表达形式；可以是某种图形；或者是一种数学表达式。' ) {
-//     score += 35;
-//   }
-//   return score;
-// };
