5392805952e1ef34faac2d02cbcf0091ed9a5ad3
xxiao <843814555@qq.com>
2014-12-18 15:58:15 -0500

    make contrastanswer

--- getScores.js
+++ getScores.js
@@ -13,11 +13,11 @@ function singleChoice() {
   var answer_2_1 = document.getElementsByName('2_1');
   var answer_2_2 = document.getElementsByName('2_2');
   for(var i = 0; i < answer_2_1.length; i++) {
-    if (answer_2_1[i].checked && answer_2_1[i].value === 'B' ) {
-      singleChoicescore += 10;
+    if (answer_2_1[i].checked ) {
+    singleChoicescore += contrastAnswer(answer_2_1[i].value,'B',10);
     }
-    if (answer_2_2[i].checked && answer_2_2[i].value === 'A' ) {
-      singleChoicescore += 10;
+    if (answer_2_2[i].checked ) {
+    singleChoicescore += contrastAnswer(answer_2_2[i].value,'A',10);
     }
   }
   return singleChoicescore;
@@ -26,55 +26,14 @@ function singleChoice() {
 
 function multipleChoice() {
   var multipleChoicescore = 0;
-  multipleChoicescore += getMultipleChoice('3_1','ABD',100);
-  multipleChoicescore += getMultipleChoice('3_2','ABC',100);
-  // var answer_3_1_A = '' ;
-  // var answer_3_1_B = '';
-  // var answer_3_1_C = '';
-  // var answer_3_1_D = '';
-  // if (document.getElementById('3_1_A').checked) {
-  //   answer_3_1_A = 'A';
-  // }
-  // if (document.getElementById('3_1_B').checked) {
-  //   answer_3_1_B = 'B';
-  // }
-  // if (document.getElementById('3_1_C').checked) {
-  //   answer_3_1_C = 'C';
-  // }
-  // if (document.getElementById('3_1_D').checked) {
-  //   answer_3_1_D = 'D';
-  // }
-  //
-  // if (answer_3_1_A + answer_3_1_B + answer_3_1_C + answer_3_1_D === 'ABD') {
-  //   multipleChoicescore += 100;
-  // }
-  //
-  // var answer_3_2_A = '';
-  // var answer_3_2_B = '';
-  // var answer_3_2_C = '';
-  // var answer_3_2_D = '';
-  // if (document.getElementById('3_2_A').checked) {
-  //   answer_3_2_A = 'A';
-  // }
-  // if (document.getElementById('3_2_B').checked) {
-  //   answer_3_2_B = 'B';
-  // }
-  // if (document.getElementById('3_2_C').checked) {
-  //   answer_3_2_C = 'C';
-  // }
-  // if (document.getElementById('3_2_D').checked) {
-  //   answer_3_2_D = 'D';
-  // }
-  //
-  // if (answer_3_2_A + answer_3_2_B + answer_3_2_C + answer_3_2_D === 'ABC') {
-  //   multipleChoicescore += 100;
-  // }
+  multipleChoicescore += getMultipleChoice('3_1','ABD');
+  multipleChoicescore += getMultipleChoice('3_2','ABC');
   return multipleChoicescore;
 
 }
 
 
-function getMultipleChoice(id,answer,score) {
+function getMultipleChoice(id,answer) {
   var getMultipleChoicescore = 0;
   var choice = document.getElementsByName(id);
   var string = '';
@@ -83,9 +42,7 @@ function getMultipleChoice(id,answer,score) {
       string += choice[i].value;
     }
   }
-  if(string === answer) {
-    getMultipleChoicescore += score;
-  }
+  getMultipleChoicescore += contrastAnswer(string,answer,100);
   return getMultipleChoicescore;
 }
 
@@ -93,14 +50,8 @@ function getMultipleChoice(id,answer,score) {
 
 function trueAndFalse() {
   var trueAndFalsescore = 0;
-  var answer_4_1 = document.getElementById('4_1');
-  var answer_4_2 = document.getElementById('4_2');
-  if (answer_4_1.value === '错' ) {
-    trueAndFalsescore += 1000;
-  }
-  if (answer_4_2.value === '对' ) {
-    trueAndFalsescore += 1000;
-  }
+  trueAndFalsescore += contrastAnswer(document.getElementById('4_1').value,'错',1000);
+  trueAndFalsescore += contrastAnswer(document.getElementById('4_2').value,'对',1000);
   return trueAndFalsescore;
 }
 
