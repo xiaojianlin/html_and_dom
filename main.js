25492612a4dff1efe9879c7b9e7fa5a624120ac6
xxiao <843814555@qq.com>
2014-12-18 17:25:41 -0500

    make html.css

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function checkform() {
-  if (document.getElementById('classes').value === '' ||
-      document.getElementById('student_number').value === '' ||
-      document.getElementById('name').value === '') {
-    alert('请填写班级、学号和名字！');
-  }
-  else {
+  // if (document.getElementById('classes').value === '' ||
+  //     document.getElementById('student_number').value === '' ||
+  //     document.getElementById('name').value === '') {
+  //   alert('请填写班级、学号和姓名！');
+  // }
+  // else {
     var scores = 0;
     scores += gapFilling();
     scores += singleChoice();
@@ -13,6 +13,6 @@ function checkform() {
     scores += question();
     var goal = document.getElementById('score');
     goal.value = scores +'  分';
-  }
+  // }
   return false;
 }
